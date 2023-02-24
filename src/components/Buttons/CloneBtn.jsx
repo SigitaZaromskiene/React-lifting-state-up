3d4949512206105ae927d50a3e267188e3c62050
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-02-24 15:21:45 +0200

    CloneBtn

--- src/components/Buttons/CloneBtn.jsx
+++ src/components/Buttons/CloneBtn.jsx
@@ -2,8 +2,7 @@ import { v4 as uuidv4, v4 } from "uuid";
 
 function CloneBtn(props) {
   const cloneHandler = (id) => {
-    const clone = props.sq.find((s) => s.id === props.sq.id);
-    props.setSq((s) => [...s, { ...clone, id: uuidv4() }]);
+    props.setSq((s) => [...s, { ...props.sq, id: uuidv4() }]);
   };
   return (
     <button className={props.className} onClick={cloneHandler}>
